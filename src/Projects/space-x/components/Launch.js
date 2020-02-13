a19e99d96df187be1de2690c0a10246d5789dcda
Gary <garyguan0713@gmail.com>
2020-02-12 21:03:04 -0800

    add links from query

--- src/Projects/space-x/components/Launch.js
+++ src/Projects/space-x/components/Launch.js
@@ -1,21 +1,27 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
+import Moment from "react-moment";
+import { Image, Button } from "react-bootstrap";
 
 const LAUNCH_QUERY = gql`
   query LaunchQuery($flight_number: Int!) {
     launch(flight_number: $flight_number) {
       flight_number
       mission_name
-      launch_year
       launch_success
       launch_date_local
       rocket {
         rocket_id
         rocket_name
         rocket_type
+      }
+      links {
+        mission_patch
+        reddit_campaign
+        article_link
       }
     }
   }
@@ -26,7 +32,7 @@ export class Launch extends Component {
     let { flight_number } = this.props.match.params;
     flight_number = parseInt(flight_number);
     return (
-      <Fragment>
+      <>
         <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
           {({ loading, error, data }) => {
             if (loading) return <h4>Loading</h4>;
@@ -35,9 +41,10 @@ export class Launch extends Component {
             const {
               mission_name,
               flight_number,
-              launch_year,
               launch_success,
-              rocket: { rocket_id, rocket_name, rocket_type }
+              launch_date_local,
+              rocket: { rocket_id, rocket_name, rocket_type },
+              links: { mission_patch, reddit_campaign, article_link }
             } = data.launch;
 
             return (
@@ -48,31 +55,44 @@ export class Launch extends Component {
                 <h1 className="display-4 my-3">
                   <span
                     className={classNames({
-                      "text-success": launch_success,
-                      "text-danger": !launch_success
+                      "text-planing": launch_success === null,
+                      "text-success": launch_success === true,
+                      "text-danger": launch_success === false
                     })}
                   >
                     Mission:
                   </span>{" "}
                   {mission_name}
                 </h1>
+                <Image
+                  style={{ width: 400, margin: "auto" }}
+                  src={mission_patch}
+                />
                 <h4 className="mb-3">Launch Details</h4>
                 <ul className="list-group">
                   <li className="list-group-item">
                     Flight Number: {flight_number}
                   </li>
                   <li className="list-group-item">
-                    Launch Year: {launch_year}
+                    Launch Date:{" "}
+                    <Moment format="YYYY-MM-DD HH:mm">
+                      {launch_date_local}
+                    </Moment>
                   </li>
                   <li className="list-group-item">
                     Launch Successful:{" "}
                     <span
                       className={classNames({
-                        "text-success": launch_success,
-                        "text-danger": !launch_success
+                        "text-planing": launch_success === null,
+                        "text-success": launch_success === true,
+                        "text-danger": launch_success === false
                       })}
                     >
-                      {launch_success ? "Yes" : "No"}
+                      {launch_success === null
+                        ? "Unknown"
+                        : launch_success
+                        ? "Yes"
+                        : "No"}
                     </span>
                   </li>
                 </ul>
@@ -86,12 +106,24 @@ export class Launch extends Component {
                     Rocket Type: {rocket_type}
                   </li>
                 </ul>
+                <h4 className="my-3">More Information</h4>
+                <Button
+                  className="mr-3"
+                  variant="success"
+                  href={reddit_campaign}
+                  target="_blank"
+                >
+                  Reddit Campagin
+                </Button>
+                <Button variant="success" href={article_link} target="_blank">
+                  Article link
+                </Button>
                 <hr />
               </div>
             );
           }}
         </Query>
-      </Fragment>
+      </>
     );
   }
 }
